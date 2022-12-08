262b22e346f70eaa23a1ff2741596b4e55f6a55b
Elippsis007 <michaeldrmm@gmail.com>
2022-12-08 15:40:29 +0000

    finished application

---
+++ JS_code/icecream_scoops.js
@@ -0,0 +1,10 @@
+// Setting value of 5 to variable scoops
+var scoops = 5;
+// While scoops value is greater than 0 it will return the string below
+while (scoops > 0) {
+    document.write("Another scoop!<br>");
+// Each time the while loops it takes 1 scoop from our value 5 until it reaches 0
+    scoops = scoops - 1;
+}
+// When the value reaches zero it will return the below string and the loop ends.
+document.write("<br>Life without ice cream isn't the same");
